a6a54ca35f9df3b76ee49517ae9ed33fdbc06fbf
Serafin <alighierisram@gmail.com>
2024-05-11 18:46:42 -0400

    add other objects

--- index.js
+++ index.js
@@ -2,19 +2,15 @@ function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
     object.style.position = 'fixed'
-    object.style.left = 'px'
-    object.style.bottom = 'px'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
     document.body.append(object)
     return object
 }
 
-
-
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)
-
-newImage(assets/green-character.gif, 100, 100)
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
