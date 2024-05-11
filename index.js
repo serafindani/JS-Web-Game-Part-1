c4cbb5a8ede43a0d58e5412dbb332d71c7836ae9
Serafin <alighierisram@gmail.com>
2024-05-11 18:26:49 -0400

    trying to refactor little dude

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = 'px'
+    object.style.bottom = 'px'
+    document.body.append(object)
+    return object
+}
+
+
+
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
+
+newImage(assets/green-character.gif, 100, 100)
