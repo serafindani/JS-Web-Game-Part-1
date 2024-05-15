f5a395ec47bdb798c87f5e7728b8010c6c605c9d
Serafin <alighierisram@gmail.com>
2024-05-15 18:38:52 -0400

    removed return so my eventlistener would work

--- index.js
+++ index.js
@@ -15,26 +15,25 @@ newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
-function newItem(url,left,bottom){
+function newItem(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
     object.style.position = 'fixed'
     object.style.left = left + 'px'
     object.style.bottom = bottom + 'px'
     document.body.append(object)
-    return object
+
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
 }
 
 newItem('assets/sword.png', 500, 405)
+newItem('assets/shield.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
 
 
-let sword = document.createElement('img')
-sword.src = 'assets/sword.png'
-sword.style.position = 'fixed'
-sword.style.left = '500px'
-sword.style.bottom = '405px'
-document.body.append(sword)
 
-sword.addEventListener('dblclick', function(){
-    sword.remove()
-})
+
+
+
